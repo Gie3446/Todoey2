980de347009fdc4148c067d405404ff2be3d4ab4
Gie3446 <virgielyn.jordan@yahoo.com>
2019-09-18 00:52:17 -0400

    Create tableView delegate method.

--- Todoey2/ToDoListViewController.swift
+++ Todoey2/ToDoListViewController.swift
@@ -17,6 +17,7 @@ class TodoListViewController : UITableViewController {
         // Do any additional setup after loading the view.
     }
     
+    //MARK Create DataSource Methods :
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
@@ -31,6 +32,26 @@ class TodoListViewController : UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return itemArray.count
+        
+    }
+    
+    //MARK Create Delegate Methods :
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        //print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+            
+        } else {
+            
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+            
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
         
     }
     
